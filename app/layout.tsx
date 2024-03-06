eaebf7b753b44c1bb141b923945589f665d6d239
acomarcho <acoxstpd@gmail.com>
2024-03-06 23:21:05 +0700

    Feat: Add navigation bar to all page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import JotaiProvider from "@/components/jotai/provider";
 import { Toaster } from "@/components/ui/sonner";
 import TanstackQueryProvider from "@/components/tanstack-query/provider";
+import Link from "next/link";
 
 const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -23,6 +24,16 @@ export default function RootLayout({
         <JotaiProvider>
           <TanstackQueryProvider>
             <Toaster richColors />
+            <div className="p-4 bg-slate-950">
+              <div className="px-8 max-w-wrapper mx-auto">
+                <Link
+                  href="/"
+                  className="font-bold text-slate-100 uppercase tracking-widest text-xl"
+                >
+                  Mailtuned
+                </Link>
+              </div>
+            </div>
             {children}
           </TanstackQueryProvider>
         </JotaiProvider>
