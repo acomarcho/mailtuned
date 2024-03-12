00668f61513e67d187ba55efe574376c6fa251fb
acomarcho <acoxstpd@gmail.com>
2024-03-12 13:44:03 +0700

    Refactor: navigation-buttons

--- components/home/navigation-buttons.tsx
+++ components/home/navigation-buttons.tsx
@@ -9,6 +9,45 @@ import { useRouter } from "next/navigation";
 const noDomainErrorMessage =
   "Please finish your API key set up before continuing!";
 
+const steps = [
+  {
+    label: "Add SPF",
+    href: "/spf",
+    className:
+      "bg-amber-200 text-amber-900 w-full uppercase font-bold tracking-widest hover:bg-amber-400",
+  },
+  {
+    label: "Add MX records",
+    href: "/mx",
+    className:
+      "bg-rose-200 text-rose-900 w-full uppercase font-bold tracking-widest hover:bg-rose-400",
+  },
+  {
+    label: "Add DKIM",
+    href: "/dkim",
+    className:
+      "bg-lime-200 text-lime-900 w-full uppercase font-bold tracking-widest hover:bg-lime-400",
+  },
+  {
+    label: "Add DMARC",
+    href: "/dmarc",
+    className:
+      "bg-purple-200 text-purple-900 w-full uppercase font-bold tracking-widest hover:bg-purple-400",
+  },
+  {
+    label: "Add tracking",
+    href: "/tracking",
+    className:
+      "bg-pink-200 text-pink-900 w-full uppercase font-bold tracking-widest hover:bg-pink-400",
+  },
+  {
+    label: "Add redirect",
+    href: "/redirect",
+    className:
+      "bg-sky-200 text-sky-900 w-full uppercase font-bold tracking-widest hover:bg-sky-400",
+  },
+];
+
 export default function NavigationButtons() {
   const domain = useAtomValue(domainAtom);
   const isDomainSelected = domain !== undefined;
@@ -29,72 +68,22 @@ export default function NavigationButtons() {
         Add All
       </Button>
       <p className="text-slate-500">or you can add them one by one!</p>
-      <Button
-        className="bg-amber-200 text-amber-900 w-full uppercase font-bold tracking-widest hover:bg-amber-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/spf");
-        }}
-      >
-        (1) Add SPF
-      </Button>
-      <Button
-        className="bg-rose-200 text-rose-900 w-full uppercase font-bold tracking-widest hover:bg-rose-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/mx");
-        }}
-      >
-        (2) Add MX records
-      </Button>
-      <Button
-        className="bg-lime-200 text-lime-900 w-full uppercase font-bold tracking-widest hover:bg-lime-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/dkim");
-        }}
-      >
-        (3) Add DKIM
-      </Button>
-      <Button
-        className="bg-purple-200 text-purple-900 w-full uppercase font-bold tracking-widest hover:bg-purple-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/dmarc");
-        }}
-      >
-        (4) Add DMARC
-      </Button>
-      <Button
-        className="bg-pink-200 text-pink-900 w-full uppercase font-bold tracking-widest hover:bg-pink-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/tracking");
-        }}
-      >
-        (5) Add tracking
-      </Button>
-      <Button
-        className="bg-sky-200 text-sky-900 w-full uppercase font-bold tracking-widest hover:bg-sky-400"
-        onClick={() => {
-          if (!isDomainSelected) {
-            return toast.error(noDomainErrorMessage);
-          }
-          router.push("/redirect");
-        }}
-      >
-        (6) Add redirect
-      </Button>
+      {steps.map((step, i) => {
+        return (
+          <Button
+            className={step.className}
+            key={step.href}
+            onClick={() => {
+              if (!isDomainSelected) {
+                return toast.error(noDomainErrorMessage);
+              }
+              router.push(step.href);
+            }}
+          >
+            ({i + 1}) {step.label}
+          </Button>
+        );
+      })}
     </>
   );
 }
